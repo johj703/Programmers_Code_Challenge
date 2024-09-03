6a5c085081c1dc7e5fab213e8f716cc60f730c48
Jo <84303533+johj703@users.noreply.github.com>
2024-09-03 09:36:20 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.19 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,25 @@
+function solution(k, score) {
+    // 명예의 전당 배열
+    const honor = [];
+    
+    // 결과 값
+    const result = [];
+    
+    // 모든 점수 순회
+    for(let i = 0; i < score.length; i++){
+        // 명예의 전당 기본 세팅
+        if(i < k) {
+            honor.push(score[i]);
+        }
+        // 최근에 받은 점수가 명예의 전당 최하위 점수보다 큰 경우
+        if(score[i] > Math.min(...honor)) {
+            // 가장 작은 수를 이번 점수와 교체 후 내림차순 정렬
+            honor.pop();
+            honor.push(score[i]);
+            honor.sort((a, b) => b-a);
+        }
+        // 결과 값에 명예의 전당 중 최하위 점수 입력
+        result.push(honor.at(-1));
+    }
+    return result;
+}
