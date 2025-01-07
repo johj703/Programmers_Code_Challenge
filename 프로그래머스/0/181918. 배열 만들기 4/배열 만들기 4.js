bae00c5147060bc4294ced986e0943de53d58977
Jo <84303533+johj703@users.noreply.github.com>
2025-01-07 21:21:26 +0900

    [level 0] Title: 배열 만들기 4, Time: 27.43 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var stk = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(stk.length === 0){
+            stk.push(arr[i]);
+        }
+        else if(stk[stk.length - 1] < arr[i]){
+            stk.push(arr[i]);
+        }
+        else if(stk[stk.length - 1] >= arr[i]){
+            stk.pop();
+            i--;
+        }
+    }
+    return stk;
+}
+
+console.log(solution([1, 4, 2, 5, 3]))
