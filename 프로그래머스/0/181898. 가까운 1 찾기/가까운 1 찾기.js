d65195be2ff1550846f142c4f81a9e46df5c7a1d
Jo <84303533+johj703@users.noreply.github.com>
2025-01-29 23:31:32 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 35.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,7 @@
+function solution(arr, idx) {
+    // indexOf(1, idx)
+    // 1: 찾으려는 숫자는 1
+    // idx: 검색을 시작할 위치
+    // 반환 값: 1을 찾았다면 1의 인덱스 반환 또는 못 찾으면 -1 반환
+    return arr.indexOf(1, idx);
+}
