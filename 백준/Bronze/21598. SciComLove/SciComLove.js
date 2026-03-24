fb8435add40014fd44ded359d1c18b0f18ae0954
Jo <84303533+johj703@users.noreply.github.com>
2026-03-24 16:50:08 +0900

    [Bronze V] Title: SciComLove, Time: 88 ms, Memory: 9324 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.js
@@ -0,0 +1,11 @@
+// ===== 입력 처리 =====
+// 실행 환경에 따라 입력 파일 경로 설정 (백준: /dev/stdin, 로컬: ./input.txt)
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+// 입력 파일을 읽고 문자열 변환 후, 앞뒤 공백 제거
+const N = Number(require('fs').readFileSync(file).toString().trim());
+
+// ===== N번 절하기 =====
+for (let i = 0; i < N; i++) {
+    console.log('SciComLove');
+}
